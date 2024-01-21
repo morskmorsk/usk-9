94c8a38226d6d20156c6cd986a3d31d26e80ebc8
morsk <khalidfour@yahoo.com>
2024-01-21 17:07:18 -0600

    Refactor code for improved performance and readability

--- mysite/store/models.py
+++ mysite/store/models.py
@@ -336,3 +336,20 @@ class Part(models.Model):
     class Meta:
         verbose_name_plural = 'Parts'
         ordering = ['id']
+
+
+class CartPayment(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=255)
+    cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE)
+    # transaction_id = models.CharField(max_length=255)
+    # notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.payment_method} Payment for Cart {self.cart.id}"
+
+    class Meta:
+        verbose_name_plural = 'Cart Payments'
+        ordering = ['id']
